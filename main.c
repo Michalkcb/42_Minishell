caf1574f2c3c2921467b642d28e17a55a6500b6c
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-12-08 14:27:14 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main(void)
+{
+    char *rl;
+    rl = readline("Prompt > ");
+    printf("%s\n", rl);
+    return (0);
+}
+/*
+Skompilowanie i uruchomienie tego programu spowoduje pojawienie się poniższych efektów.
+
+Kopia
+$> ./minishell
+Prompt > Hi ! How are you ?
+Hi ! How are you ?
+$>
+*/
