b94823775f771a91574049ec8065ddad2b70099e
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-12-21 19:09:17 +0100

    init header

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,1 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 18:19:19 by mbany             #+#    #+#             */
+/*   Updated: 2024/12/21 18:39:52 by mbany            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdlib.h>
+
+typedef struct s_envp
+{
+	char			*value;
+	struct s_envp	*next;
+}	t_envp;
+
+typedef struct s_cmd
+{
+	char			**cmd;
+	char			*infile;
+	char			*outfile;
+	bool			append;
+	bool			here_doc;
+	bool			redir_error;
+	struct s_cmd	*next;
+}	t_cmd;
+
+typedef struct s_data
+{
+	t_cmd		*cmd;
+	t_envp		*envp;
+	char		**envp_arr;
+	char		*line;
+	int			cmd_exit_status;
+}	t_data;
+
+/* errors */
+# define MANY_ARGS_ERR "Error: minishell does not accept arguments"
+# define NO_ENVP_ERR "Error: no environment found"
+
+/* Print a message describing the meaning of the value of errno.
+
+   This function is a possible cancellation point and therefore not
+   marked with __THROW.  */
+extern void perror (const char *__s);
+
+#endif
