d4f4d075abd50bb421c6034b682c308370f01e9a
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-12-29 16:11:50 +0100

    update

--- src/error.c
+++ src/error.c
@@ -6,11 +6,16 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 15:24:26 by mbany             #+#    #+#             */
-/*   Updated: 2024/12/28 15:25:56 by mbany            ###   ########.fr       */
+/*   Updated: 2024/12/29 14:37:46 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+void	msg_error(char *err)
+{
+	write(2, err, ft_strlen(err));
+	write(2, "\n", 1);
+}
 
 int	ft_error_message(char *str, int num)
 {
