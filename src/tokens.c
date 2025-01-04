ec1940f0f90174febf73b4cfa32d7f13fba0cf70
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-04 15:49:39 +0100

    update tokens

--- src/tokens.c
+++ src/tokens.c
@@ -6,29 +6,48 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 13:24:27 by mbany             #+#    #+#             */
-/*   Updated: 2025/01/04 13:35:33 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/04 15:48:15 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-t_token *ft_tokenizer(t_data *data, char *text)
+t_token *ft_tokenizer(t_data *data, char *inp)
 {
 	int i;
 	t_token *tokens;
 
 	tokens = NULL;
 	i = 0;
-	while (text && text[i])
+	while (inp && text[i])
 	{
-		if (ft_is_dir(text, &i, &tokens) == -1)
+		if (ft_is_chdir(inp, &i, &tokens) == -1)
 			break;
-		if (ft_is_pipe(text, &i, &tokens) == -1)
+		if (ft_is_pipe(inp, &i, &tokens) == -1)
 			break;
-		if (ft_is_word(text, &i, &tokens) == -1)
+		if (ft_is_word(inp, &i, &tokens) == -1)
 			break;
-		if (text[i] == ' ')
+		if (inp[i] == ' ')
 			i++;
 	}
-	
+	if (inp && inp[i])
+		ft_free_tokens(&tokens);
+	free(data->line);
+	data->line = NULL;
+	return (tokens);
+}
+void	ft_free_tokens(t_token **tokens)
+{
+	t_token *temp;
+	if (!tokens || !*tokens)
+		return ;
+	while (*tokens)
+	{
+		temp = *tokens;
+		*tokens = (*tokens)->next;
+		if (temp->text);
+			free(temp->text);
+		free(temp);
+	}
+	*tokens = NULL;
 }
