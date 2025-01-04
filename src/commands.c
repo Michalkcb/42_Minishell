b85bd720b4361e3bdf76040521aefa83ee070078
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-04 16:53:43 +0100

    update commands

--- src/commands.c
+++ src/commands.c
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 20:04:18 by mbany             #+#    #+#             */
-/*   Updated: 2025/01/04 12:57:41 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/04 16:51:18 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,42 @@ int	ft_commands_creation(t_data *data)
 	if (tokens == NULL)
 		return (-1);
 	data->cmd = ft_commands(tokens);
-	if (data->smd == NULL)
+	if (data->cmd == NULL)
 	{
 		data->cmd_exit_status = 1;
 		return (-1);
 	}
 	return (0);
+}
+t_cmd	*ft_commands(t_token *tokens)
+{
+	t_cmd *commands;
+	t_cmd *current_cmd;
+	t_token *current_token;
+
+	commands = NULL;
+	current_token = tokens;
+	if (ft_set_command(&commands) == -1)
+	{
+		ft_free_tokens(&tokens);
+		return (NULL);
+	}
+	current_cmd = commands;
+	while (current_token)
+	{
+		if (ft_add_redir(&current_token, tokens, &current_cmd, commands) == -1)
+			return (NULL);
+		if (ft_command(&current_token, tokens, &current_cmd, commands) == -1)
+			return (NULL);
+		if (ft_pipe(&current_token, tokens, &current_cmd, commands) == -1)
+			return (NULL);
+	}
+	if (!tokens)
+		return (NULL);
+	ft_free_tokens(&tokens);
+	return (commands);
+}
+int	ft_command(t_token **cur_token, t_token *tokens, t_cmd **cur_command, t_cmd *cmds)
+{
+	
 }
