afe33e93cb372ed202036e4f004a221d33cccc7d
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-04 12:59:47 +0100

    add int	ft_commands_creation(t_data *data)

--- src/commands.c
+++ src/commands.c
@@ -6,13 +6,34 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 20:04:18 by mbany             #+#    #+#             */
-/*   Updated: 2025/01/02 20:05:56 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/04 12:57:41 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	ft_cmds_creation(t_data *data)
+int	ft_commands_creation(t_data *data)
 {
+	t_token *tokens;
 
+	tokens = ft_tokenizer(data, data->line);
+	if(tokens == NULL)
+	{
+		data->cmd_exit_status = 1;
+		return (-1);
+	}
+	if (ft_check_tokens(&tokens) == -1)
+	{
+		data->cmd_exit_status = 1;
+		return (-1);
+	}
+	if (tokens == NULL)
+		return (-1);
+	data->cmd = ft_commands(tokens);
+	if (data->smd == NULL)
+	{
+		data->cmd_exit_status = 1;
+		return (-1);
+	}
+	return (0);
 }
