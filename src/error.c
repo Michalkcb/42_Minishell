5dbf0a5d46b8092022613a0d5cfd2398c98bbe58
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-05 15:35:14 +0100

    add ft_perror_message

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 15:24:26 by mbany             #+#    #+#             */
-/*   Updated: 2025/01/05 13:23:23 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/05 15:34:44 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,20 @@ int	ft_error_message(char *str, int num)
 int	ft_perror_message(void)
 {
 	perror(NULL);
+	return (-1);
+}
+
+/*
+Funkcja `ft_perror_free` wypisuje komunikat o błędzie za pomocą `ft_perror_message`, a następnie zwalnia pamięć wskazywaną przez wskaźniki `first`, `second` i `third`, jeśli nie są one puste. Na końcu zwraca `-1`, aby zasygnalizować błąd. Funkcja służy do obsługi błędów i jednoczesnego czyszczenia zaalokowanej pamięci, zapobiegając wyciekom.
+*/
+int	ft_perror_free(char *first, char *second, char *third)
+{
+	ft_perror_message();
+	if (first)
+		free(first);
+	if (second)
+		free(second);
+	if (third)
+		free(third);
 	return (-1);
 }
