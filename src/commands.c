2ea7771f91d26c593ee36a28f911b80d5bd7d4bd
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-05 13:12:50 +0100

    update

--- src/commands.c
+++ src/commands.c
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 20:04:18 by mbany             #+#    #+#             */
-/*   Updated: 2025/01/04 16:51:18 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/05 13:04:54 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ t_cmd	*ft_commands(t_token *tokens)
 	current_cmd = commands;
 	while (current_token)
 	{
-		if (ft_add_redir(&current_token, tokens, &current_cmd, commands) == -1)
+		if (ft_redir(&current_token, tokens, &current_cmd, commands) == -1)
 			return (NULL);
 		if (ft_command(&current_token, tokens, &current_cmd, commands) == -1)
 			return (NULL);
@@ -65,7 +65,59 @@ t_cmd	*ft_commands(t_token *tokens)
 	ft_free_tokens(&tokens);
 	return (commands);
 }
-int	ft_command(t_token **cur_token, t_token *tokens, t_cmd **cur_command, t_cmd *cmds)
+int	ft_set_command(t_cmd **commands)
+{
+	t_cmd *new;
+	t_cmd *tmp;
+
+	new = malloc(sizeof(t_cmd));
+	if (!new)
+		return (ft_perror_message());
+	new->next = NULL;
+	new->append = false;
+	new->infile = NULL;
+	new->outfile = NULL;
+	new->cmd = NULL;
+	new->redir_error = false;
+	new->here_doc  =false;
+	if (!(*commands))
+		*commands = new;
+	else
+	{
+		tmp = *commands;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+	return (0);
+}
+int	ft_redir(t_token **current_tok, t_token *head_tok,t_cmd **current_cmd, t_cmd *head_cmd)
+{
+	if (!*current_tok)
+		return (0);
+	if ((*current_tok)->type != T_WORD && (*current_tok)->type != T_PIPE)
+	{
+		if ((*current_tok)->next && (*current_tok)->next == T_WORD)
+		{
+			if (ft_set_redir(current_tok, *current_cmd) == -1)
+			{
+				ft_free_tokens(&head_tok);
+				ft_free_commands(&head_cmd);
+				return (-1);
+			}
+			(*current_tok) = (*current_tok)->next->next;
+		}
+		else
+		{
+			ft_free_tokens(&head_tok);
+			ft_free_commands(&head_cmd);
+			return (ft_error_message(REDIR_TO_OPR, -1));
+		}
+	}
+	return (0);
+}
+
+int	ft_command(t_token **cut_token, t_token *tokens, t_cmd **cur_command, t_cmd *cmds)
 {
 	
 }
