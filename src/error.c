1ed7d0ebcfbf4b3b8335d937c8943bcaa5d2b904
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-05 13:24:52 +0100

    add perror

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 15:24:26 by mbany             #+#    #+#             */
-/*   Updated: 2024/12/29 14:37:46 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/05 13:23:23 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,9 @@ int	ft_error_message(char *str, int num)
 	ft_putstr_fd(str, 2);
 	ft_putstr_fd("\n", 2);
 	return (num);
+}
+int	ft_perror_message(void)
+{
+	perror(NULL);
+	return (-1);
 }
