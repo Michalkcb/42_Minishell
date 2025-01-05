fdb0669482770f7c1e0d040d6a786fd48615713b
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-05 13:13:26 +0100

    update

--- src/tokens2.c
+++ src/tokens2.c
@@ -6,27 +6,50 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 15:09:41 by mbany             #+#    #+#             */
-/*   Updated: 2025/01/04 16:36:12 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/05 11:34:47 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int ft_is_chdir(char *inp,int &i,t_token &tokens)
+static int	ft_append_redir(char *input, int *i, t_token **tokens, char *str)
 {
-	int error;
-	char *str;
+	int	error;
+
+	if (input [*i] == '<')
+		str = ft_strdup("<<");
+	else
+		str = ft_strdup(">>");
+	if (!str)
+		return (ft_perror_message());
+	if (input[*i] == '<')
+		error = create_token(str, T_HEREDOC, tokens);
+	else
+		error = create_token(str, T_APPEND, tokens);
+	if (error == -1)
+	{
+		free(str);
+		return (-1);
+	}
+	*i = *i + 2;
+	return (0);
+}
+
+int	ft_is_redir(char *input, int *i, t_token **tokens)
+{
+	int		error;
+	char	*str;
 
 	str = NULL;
-	if (ft_strchr("<>", inp[*i]))
+	if (ft_strchr("<>", input[*i]))
 	{
-		if (inp[*i] == inp[*i +1])
+		if (input[*i] == input [*i + 1])
 		{
-			if (ft_add_redir(inp, i, tokens, str) == -1)
+			if (ft_append_redir(input, i, tokens, str) == -1)
 				return (-1);
 			return (0);
 		}
-		error = ft_single_redir(inp[*i], tokens, str);
+		error = ft_single_redirection(input [*i], tokens, str);
 		if (error != 0)
 			return (-1);
 		(*i)++;
@@ -44,25 +67,4 @@ ft_is_word(inp, &i, &tokens)
 	
 }
 
-static int ft_add_redir(char *inp, int *i, t_token **tokens, char *str)
-{
-	int error;
 
-	if (inp[*i] == '<')
-		str = ft_strdup("<<");
-	else
-		str = ft_strdup(">>");
-	if (!str)
-		return (ft_perror_message());
-	if (inp[*i] == '<')
-		error = create_token(str, T_HEREDOC, tokens);
-	else
-		error = create_token(str, T_APPEND, tokens);
-	if (error == -1)
-	{
-		free(str);
-		return (-1);
-	}
-	*i = *i + 2;
-	return (0);		
-}
