5c843a529f3d1e32c7692b5e4eac3d010d96a712
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-07 19:39:13 +0100

    add check for buildins

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 18:08:10 by mbany             #+#    #+#             */
-/*   Updated: 2025/01/02 20:03:26 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/07 19:38:25 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,26 @@ void	init(t_data *data,int argc,char **argv,char **envp)
 	data->line = NULL;
 	data->cmd_exit_status = 0;
 }
+/*
+Funkcja `check_for_builtins` sprawdza, czy komenda w strukturze `data->cmd` to jedna z komend wbudowanych (jak `exit`, `export`, `unset`, czy `cd`). Jeśli tak, wywołuje odpowiednią funkcję wbudowaną, a w przypadku `exit` kończy program, w innych przypadkach ustawia status zakończenia komendy (`data->cmd_exit_status`). Jeśli nie ma komendy lub występuje komenda kolejna (`next`), funkcja nie wykonuje żadnych działań.
+*/
+void	check_for_builtins(t_data *data)
+{
+	if (data->cmd->next != NULL || !data->cmd->cmd)
+		return ;
+	if (data->cmd->cmd && ft_strncmp(data->cmd->cmd[0],
+			"exit", 5) == 0)
+		exit_bltin(data);
+	else if (ft_strncmp(data->cmd->cmd[0],
+			"export", ft_strlen(data->cmd->cmd[0])) == 0)
+		data->cmd_exit_status = export_bltin(data->cmd->cmd, data);
+	else if (ft_strncmp(data->cmd->cmd[0],
+			"unset", ft_strlen(data->cmd->cmd[0])) == 0)
+		data->cmd_exit_status = unset_bltin(data->cmd->cmd, data);
+	else if (ft_strncmp(data->cmd->cmd[0],
+			"cd", ft_strlen(data->cmd->cmd[0])) == 0)
+		data->cmd_exit_status = cd_bltin(data->cmd->cmd, data);
+}
 
 /*envp == NULL we check if we have environment variables*/
 
@@ -89,6 +109,7 @@ int	main(int argc, char **argv, char **envp)
 			continue ;
 		if (ft_commands_creation(&data) == -1)
 			continue ;
+		check_for_builtins(&data);
 	}
 
 }
