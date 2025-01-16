10d1f2991c54e28250022bce52f0e5f431591051
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-16 19:53:39 +0100

    add ft_cd_home

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 19:34:11 by mbany             #+#    #+#             */
-/*   Updated: 2025/01/14 19:37:17 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/16 19:52:58 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,37 @@ int	cd_bltin(char **cmd, t_data *data)
 		return (ft_cd_home(data, node));
 	}
 	else if (cd_handler(cmd[1], data) == -1)
+		return (1);
+	return (0);
+}
+/*
+Funkcja `ft_cd_home` zmienia aktualny katalog roboczy na katalog domowy użytkownika zapisany w zmiennej środowiskowej `HOME`. Jeśli `HOME` nie jest ustawiona, funkcja zwraca błąd. Po zmianie katalogu aktualizuje zmienne środowiskowe `OLDPWD` i `PWD`, które śledzą poprzedni oraz obecny katalog roboczy. Jest kluczowa w projekcie *Minishell*, ponieważ implementuje obsługę komendy `cd` bez argumentów, umożliwiając użytkownikowi szybki powrót do katalogu domowego zgodnie z zachowaniem standardowych powłok systemowych.
+*/
+static int	ft_cd_home(t_data *data, t_envp *home)
+{
+	t_envp	*node_pwd;
+	char	curr[4096];
+
+	if (!home)
+		return (ft_error_message("HOME not set", 1));
+	if (chdir(&home->value[5]))
+	{
+		ft_perror_message();
+		return (1);
+	}
+	getcwd(curr, 4096);
+	node_pwd = fetch_envp_node(data->envp, "PWD");
+	if (!home)
+	{
+		if (ft_change_value("OLDPWD", NULL, data) == -1)
+			return (1);
+	}
+	else
+	{
+		if (ft_change_value("OLDPWD", &node_pwd->value[4], data) == -1)
+			return (1);
+	}
+	if (ft_change_value("PWD", curr, data) == -1)
 		return (1);
 	return (0);
 }
