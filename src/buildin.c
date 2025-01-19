9c012e9262f358157bbcf0e3bd2e31a9de065cca
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-19 14:12:18 +0100

    add check_for_builtin_and_execute

--- src/buildin.c
+++ src/buildin.c
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 19:40:51 by mbany             #+#    #+#             */
-/*   Updated: 2025/01/14 19:08:32 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/19 14:08:23 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,4 +40,27 @@ void	exit_bltin(t_data *data)
 		exit_status = data->cmd_exit_status;
 	free_resources(data);
 	exit(exit_status);
+}
+/*
+Funkcja `check_for_builtin_and_execute` sprawdza, czy polecenie przekazane jako argument `cmd` jest jednym z wbudowanych poleceń powłoki, takich jak `exit`, `env`, `echo`, `pwd`, `cd`, `unset`, czy `export`, i wykonuje odpowiednią funkcję implementującą dane polecenie. Jeśli polecenie jest wbudowane, zostaje ono obsłużone natychmiast w procesie rodzica (bez tworzenia procesu podrzędnego). Funkcja zwraca `-1`, jeśli brak polecenia, a `0`, jeśli wykonano wbudowane polecenie. Jest to kluczowe w projekcie *minishell*, aby obsłużyć wbudowane polecenia zgodnie z wymaganiami i bez uruchamiania ich jako oddzielnych procesów, co różni je od poleceń zewnętrznych. 
+*/
+int	check_for_builtin_and_execute(char **cmd, t_data *data)
+{
+	if (!cmd || !cmd[0])
+		return (-1);
+	if (ft_strncmp(cmd[0], "exit", ft_strlen(cmd[0])) == 0)
+		exit_bltin(data);
+	else if (ft_strncmp(cmd[0], "env", ft_strlen(cmd[0])) == 0)
+		env_bltin(data);
+	else if (ft_strncmp(cmd[0], "echo", ft_strlen(cmd[0])) == 0)
+		echo_bltin(cmd);
+	else if (ft_strncmp(cmd[0], "pwd", ft_strlen(cmd[0])) == 0)
+		pwd_bltin();
+	else if (ft_strncmp(cmd[0], "cd", ft_strlen(cmd[0])) == 0)
+		exit (data->cmd_exit_status);
+	else if (ft_strncmp(cmd[0], "unset", ft_strlen(cmd[0])) == 0)
+		exit(data->cmd_exit_status);
+	else if (ft_strncmp(cmd[0], "export", ft_strlen(cmd[0])) == 0)
+		exit(ft_print_env_var(data));
+	return (0);
 }
