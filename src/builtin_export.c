c9f82336985088a28572580b3a91bbfb4edf150c
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-23 19:55:55 +0100

    norminette ok

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -6,14 +6,23 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 19:12:08 by mbany             #+#    #+#             */
-/*   Updated: 2025/01/19 15:24:45 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/23 19:54:52 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
 static int	ft_change_env(char *var, int j, t_data *data);
+
 /*
-Funkcja `export_bltin` obsługuje polecenie `export` wbudowane w shellu. Sprawdza, czy zostały podane zmienne do eksportu i czy mają poprawny format (rozpoczynają się literą lub podkreślnikiem, a pozostałe znaki przed `=` są alfanumeryczne lub podkreśleniem). Jeśli zmienna jest niepoprawna, wyświetla komunikat o błędzie i zwraca `1`. Dla poprawnych zmiennych wywołuje `ft_change_env`, aby dodać je do środowiska. Funkcja kończy się sukcesem (`0`) lub błędem (`1`) w przypadku niepoprawnych zmiennych lub innych problemów.
+Funkcja `export_bltin` obsługuje polecenie `export` wbudowane w shellu. 
+Sprawdza, czy zostały podane zmienne do eksportu i czy mają poprawny format 
+(rozpoczynają się literą lub podkreślnikiem, 
+a pozostałe znaki przed `=` są alfanumeryczne lub podkreśleniem). 
+Jeśli zmienna jest niepoprawna, wyświetla komunikat o błędzie i zwraca `1`. 
+Dla poprawnych zmiennych wywołuje `ft_change_env`, aby dodać je do środowiska. 
+Funkcja kończy się sukcesem (`0`) lub błędem (`1`) 
+w przypadku niepoprawnych zmiennych lub innych problemów.
 */
 int	export_bltin(char **cmd, t_data *data)
 {
@@ -42,8 +51,20 @@ int	export_bltin(char **cmd, t_data *data)
 	}
 	return (0);
 }
+
 /*
-Funkcja `ft_change_env` zajmuje się modyfikowaniem lub dodawaniem zmiennych środowiskowych. Sprawdza, czy przekazana zmienna zawiera znak `=`, oddzielający nazwę zmiennej od jej wartości. Następnie, przy użyciu `ft_substr`, wyodrębnia nazwę zmiennej. Jeżeli zmienna już istnieje w `envp`, jej wartość jest zastępowana nową (przekazaną w `var`). Jeśli zmienna nie istnieje, jest dodawana do listy `envp`. W przypadku błędu przy alokacji pamięci lub operacjach na liście, funkcja zwraca `-1`. Po przetworzeniu zmiennej odpowiednio zwalnia pamięć dla nazw zmiennych i przypisuje nowe wartości.
+Funkcja `ft_change_env` zajmuje się modyfikowaniem 
+lub dodawaniem zmiennych środowiskowych. Sprawdza, 
+czy przekazana zmienna zawiera znak `=`, 
+oddzielający nazwę zmiennej od jej wartości. 
+Następnie, przy użyciu `ft_substr`, wyodrębnia nazwę zmiennej. 
+Jeżeli zmienna już istnieje w `envp`, 
+jej wartość jest zastępowana nową (przekazaną w `var`). 
+Jeśli zmienna nie istnieje, jest dodawana do listy `envp`. 
+W przypadku błędu przy alokacji pamięci lub operacjach na liście, 
+funkcja zwraca `-1`. 
+Po przetworzeniu zmiennej odpowiednio zwalnia pamięć 
+dla nazw zmiennych i przypisuje nowe wartości.
 */
 static int	ft_change_env(char *var, int j, t_data *data)
 {
@@ -71,8 +92,16 @@ static int	ft_change_env(char *var, int j, t_data *data)
 	node->value = dup_var;
 	return (0);
 }
+
 /*
-Funkcja `ft_print_env_var` realizuje działanie wbudowanej komendy `export`, wypisując wszystkie zmienne środowiskowe przechowywane w strukturze `envp` w formacie `declare -x VAR=value`. Jeśli przekazano argument do komendy, funkcja kończy działanie z kodem `cmd_exit_status`. W przeciwnym razie przechodzi po wszystkich zmiennych środowiskowych i wypisuje je. Funkcja kończy działanie z kodem `0`. Jest to część projektu *minishell*, odwzorowująca funkcjonalność komendy `export` w tym interpreteurze.
+Funkcja `ft_print_env_var` realizuje działanie wbudowanej komendy `export`, 
+wypisując wszystkie zmienne środowiskowe przechowywane w strukturze `envp` 
+w formacie `declare -x VAR=value`. Jeśli przekazano argument do komendy, 
+funkcja kończy działanie z kodem `cmd_exit_status`. 
+W przeciwnym razie przechodzi po wszystkich zmiennych środowiskowych 
+i wypisuje je. Funkcja kończy działanie z kodem `0`. 
+Jest to część projektu *minishell*, 
+odwzorowująca funkcjonalność komendy `export` w tym interpreteurze.
 */
 int	ft_print_env_var(t_data *data)
 {
