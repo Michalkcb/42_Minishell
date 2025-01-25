72c465b6681b7a6e8a1dadd94f641446528d987b
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-25 16:29:48 +0100

    norminette ok but too many functions

--- src/tokens2.c
+++ src/tokens2.c
@@ -6,14 +6,24 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 15:09:41 by mbany             #+#    #+#             */
-/*   Updated: 2025/01/12 15:12:15 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/25 16:29:09 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
 static int	ft_single_redirection(char x, t_token **tokens, char *str);
+
 /*
-Funkcja `ft_append_redir` sprawdza, czy znak w `input[*i]` to znak `<` lub `>`, a następnie przypisuje do zmiennej `str` odpowiednią wartość `"<<"` lub `">>"`. Jeśli alokacja pamięci dla `str` nie powiedzie się, zwraca błąd. Funkcja tworzy token dla typu `T_HEREDOC` lub `T_APPEND` za pomocą funkcji `create_token`, a następnie przesuwa indeks `*i` o 2. Jeśli wystąpi błąd podczas tworzenia tokenu, zwraca `-1`. Celem funkcji jest obsługa różnych rodzajów redirekcji (`<<` lub `>>`).
+Funkcja `ft_append_redir` sprawdza, 
+czy znak w `input[*i]` to znak `<` lub `>`, 
+a następnie przypisuje do zmiennej `str` odpowiednią wartość `"<<"` 
+lub `">>"`. Jeśli alokacja pamięci dla `str` nie powiedzie się, zwraca błąd. 
+Funkcja tworzy token dla typu `T_HEREDOC` 
+lub `T_APPEND` za pomocą funkcji `create_token`, 
+a następnie przesuwa indeks `*i` o 2. 
+Jeśli wystąpi błąd podczas tworzenia tokenu, zwraca `-1`. 
+Celem funkcji jest obsługa różnych rodzajów redirekcji (`<<` lub `>>`).
 */
 static int	ft_append_redir(char *input, int *i, t_token **tokens, char *str)
 {
@@ -37,8 +47,17 @@ static int	ft_append_redir(char *input, int *i, t_token **tokens, char *str)
 	*i = *i + 2;
 	return (0);
 }
+
 /*
-Funkcja `ft_is_redir` sprawdza, czy znak w łańcuchu `input` pod indeksem `*i` to znak redirekcji (`<` lub `>`). W przypadku podwójnego znaku redirekcji (`<<` lub `>>`) wywołuje `ft_append_redir`, tworząc odpowiedni token. W przypadku pojedynczej redirekcji tworzy token za pomocą `ft_single_redirection` i przesuwa indeks `*i`. W razie błędu zwraca `-1`, a w przeciwnym razie `0`, pozwalając kontynuować analizę tokenów. Funkcja identyfikuje redirekcje i dodaje je do listy tokenów.
+Funkcja `ft_is_redir` sprawdza, 
+czy znak w łańcuchu `input` pod indeksem `*i` to znak redirekcji (`<` lub `>`). 
+W przypadku podwójnego znaku redirekcji 
+(`<<` lub `>>`) wywołuje `ft_append_redir`, tworząc odpowiedni token. 
+W przypadku pojedynczej redirekcji tworzy token 
+za pomocą `ft_single_redirection` 
+i przesuwa indeks `*i`. W razie błędu zwraca `-1`, a w przeciwnym razie `0`, 
+pozwalając kontynuować analizę tokenów. Funkcja identyfikuje redirekcje 
+i dodaje je do listy tokenów.
 */
 int	ft_is_redir(char *input, int *i, t_token **tokens)
 {
@@ -62,8 +81,21 @@ int	ft_is_redir(char *input, int *i, t_token **tokens)
 	}
 	return (0);
 }
+
 /*
-Funkcja `ft_is_pipe` sprawdza, czy w danym miejscu ciągu wejściowego `input` (indeks wskazany przez `*i`) znajduje się znak `|`. Jeśli tak, kopiuje ten znak do nowo utworzonego ciągu `str` za pomocą `ft_strdup`, a następnie tworzy nowy token typu `T_PIPE`, przekazując go do listy tokenów za pomocą `create_token`. W przypadku błędów (np. brak pamięci podczas tworzenia `str` lub niepowodzenie w `create_token`), funkcja obsługuje błąd przez zwrócenie odpowiedniej wartości (`-1`). Gdy token zostanie utworzony pomyślnie, funkcja zwiększa indeks `*i` (pomija przetworzony znak w `input`) i zwraca `0`. Funkcja działa, by identyfikować znaki `|` jako operator rurociągu w procesie tokenizacji.
+Funkcja `ft_is_pipe` sprawdza, 
+czy w danym miejscu ciągu wejściowego `input` 
+(indeks wskazany przez `*i`) znajduje się znak `|`. 
+Jeśli tak, kopiuje ten znak do nowo utworzonego ciągu `str` 
+za pomocą `ft_strdup`, a następnie tworzy nowy token typu `T_PIPE`, 
+przekazując go do listy tokenów za pomocą `create_token`. 
+W przypadku błędów (np. brak pamięci podczas tworzenia `str` 
+lub niepowodzenie w `create_token`), 
+funkcja obsługuje błąd przez zwrócenie odpowiedniej wartości (`-1`). 
+Gdy token zostanie utworzony pomyślnie, 
+funkcja zwiększa indeks `*i` (pomija przetworzony znak w `input`) 
+i zwraca `0`. Funkcja działa, 
+by identyfikować znaki `|` jako operator rurociągu w procesie tokenizacji.
 */
 int	ft_is_pipe(char *input, int *i, t_token **tokens)
 {
@@ -85,8 +117,17 @@ int	ft_is_pipe(char *input, int *i, t_token **tokens)
 	}
 	return (0);
 }
+
 /*
-Funkcja `ft_is_word` sprawdza, czy znak w ciągu wejściowym `input` na pozycji `*i` nie jest jednym ze znaków specjalnych `|`, `<`, `>` lub spacji. Jeśli warunek jest spełniony, wywołuje funkcję `ft_create_word_tok`, która tworzy token typu "słowo" i dodaje go do listy tokenów. W przypadku błędu w `ft_create_word_tok` funkcja zwalnia pamięć przydzieloną dla `input` i zwraca `-1`, aby zasygnalizować niepowodzenie. W przeciwnym razie funkcja zwraca `0`. Ma na celu identyfikację i tokenizację zwykłych słów w procesie analizy leksykalnej wejścia.*/
+Funkcja `ft_is_word` sprawdza, 
+czy znak w ciągu wejściowym `input` na pozycji `*i` 
+nie jest jednym ze znaków specjalnych `|`, `<`, `>` lub spacji. 
+Jeśli warunek jest spełniony, wywołuje funkcję `ft_create_word_tok`, 
+która tworzy token typu "słowo" i dodaje go do listy tokenów. 
+W przypadku błędu w `ft_create_word_tok` funkcja zwalnia pamięć 
+przydzieloną dla `input` i zwraca `-1`, aby zasygnalizować niepowodzenie. 
+W przeciwnym razie funkcja zwraca `0`. Ma na celu identyfikację 
+i tokenizację zwykłych słów w procesie analizy leksykalnej wejścia.*/
 int	ft_is_word(char *input, int *i, t_token **tokens, t_data *data)
 {
 	if (!(ft_strchr("| <>", input[*i])))
@@ -99,8 +140,19 @@ int	ft_is_word(char *input, int *i, t_token **tokens, t_data *data)
 	}
 	return (0);
 }
+
 /*
-Funkcja `ft_single_redirection` obsługuje pojedyncze znaki przekierowań wejścia (`<`) lub wyjścia (`>`), tworząc odpowiedni token. Na podstawie znaku `x` przypisuje odpowiedni typ (`T_IN_REDIR` lub `T_OUT_REDIR`) oraz kopiuje znak do nowego łańcucha za pomocą `ft_strdup`. Następnie wywołuje `create_token`, aby dodać nowy token do listy `tokens`. Funkcja zwraca `0` w przypadku powodzenia lub `-1`, jeśli wystąpi błąd (np. brak pamięci). Jej celem jest poprawne rozpoznawanie i dodawanie tokenów dla operatorów przekierowań w analizatorze leksykalnym.
+Funkcja `ft_single_redirection` obsługuje pojedyncze znaki 
+przekierowań wejścia (`<`) lub wyjścia (`>`), tworząc odpowiedni token. 
+Na podstawie znaku `x` przypisuje odpowiedni typ 
+(`T_IN_REDIR` lub `T_OUT_REDIR`) 
+oraz kopiuje znak do nowego łańcucha za pomocą `ft_strdup`. 
+Następnie wywołuje `create_token`, 
+aby dodać nowy token do listy `tokens`. 
+Funkcja zwraca `0` w przypadku powodzenia lub `-1`, 
+jeśli wystąpi błąd (np. brak pamięci). 
+Jej celem jest poprawne rozpoznawanie 
+i dodawanie tokenów dla operatorów przekierowań w analizatorze leksykalnym.
 */
 static int	ft_single_redirection(char x, t_token **tokens, char *str)
 {
@@ -127,8 +179,21 @@ static int	ft_single_redirection(char x, t_token **tokens, char *str)
 	}
 	return (0);
 }
+
 /*
-Funkcja `ft_create_word_tok` tworzy token reprezentujący słowo w ciągu wejściowym `str`, zaczynając od pozycji `*i`. Oblicza długość słowa `n`, uwzględniając sekwencje znaków wewnątrz pojedynczych (`'`) i podwójnych (`"`) cudzysłowów, oraz ignoruje spacje jako separatory. Gdy długość słowa zostanie wyznaczona, wyodrębnia jego treść i tworzy odpowiedni token za pomocą `ft_extract_word`. Po zakończeniu aktualizuje wartość `*i`, przesuwając wskaźnik na koniec przetworzonego słowa. Funkcja zwraca `0` w przypadku sukcesu lub `-1`, gdy wystąpi błąd. Celem jest poprawne przetwarzanie słów w analizatorze leksykalnym.
+Funkcja `ft_create_word_tok` tworzy token reprezentujący 
+słowo w ciągu wejściowym `str`, 
+zaczynając od pozycji `*i`. 
+Oblicza długość słowa `n`, 
+uwzględniając sekwencje znaków wewnątrz pojedynczych (`'`) 
+i podwójnych (`"`) cudzysłowów, oraz ignoruje spacje jako separatory. 
+Gdy długość słowa zostanie wyznaczona, wyodrębnia jego treść 
+i tworzy odpowiedni token za pomocą `ft_extract_word`. 
+Po zakończeniu aktualizuje wartość `*i`, 
+przesuwając wskaźnik na koniec przetworzonego słowa. 
+Funkcja zwraca `0` w przypadku sukcesu lub `-1`, 
+gdy wystąpi błąd. Celem jest poprawne przetwarzanie 
+słów w analizatorze leksykalnym.
 */
 
 int	ft_create_word_tok(char *str, int *i, t_token **tokens, t_data *data)
@@ -158,8 +223,17 @@ int	ft_create_word_tok(char *str, int *i, t_token **tokens, t_data *data)
 	*i = *i + n;
 	return (0);
 }
+
 /*
-Funkcja `ft_extract_word` tworzy nowy token typu słowo (T_WORD) na podstawie ciągu znaków `str` o długości `*n`, alokując pamięć na nowy ciąg `word` i kopiując do niego odpowiedni fragment. Następnie sprawdza obecność symbolu `$` za pomocą `ft_check_for_dollar`, a później przetwarza słowo funkcją `ft_cross_word`. Jeśli żadna z tych operacji nie zwróci błędu, tworzy nowy token i dodaje go do listy `tokens`. Funkcja umożliwia rozdzielanie słów na tokeny przy analizie leksykalnej, z uwzględnieniem zmiennych środowiskowych i specjalnych modyfikacji.
+Funkcja `ft_extract_word` tworzy nowy token typu słowo 
+(T_WORD) na podstawie ciągu znaków `str` o długości `*n`, 
+alokując pamięć na nowy ciąg `word` i kopiując do niego odpowiedni fragment. 
+Następnie sprawdza obecność symbolu `$` za pomocą `ft_check_for_dollar`, 
+a później przetwarza słowo funkcją `ft_cross_word`. 
+Jeśli żadna z tych operacji nie zwróci błędu, 
+tworzy nowy token i dodaje go do listy `tokens`. 
+Funkcja umożliwia rozdzielanie słów na tokeny przy analizie leksykalnej, 
+z uwzględnieniem zmiennych środowiskowych i specjalnych modyfikacji.
 */
 int	ft_extract_word(char *str, int *n, t_token **tokens, t_data *data)
 {
@@ -187,8 +261,18 @@ int	ft_extract_word(char *str, int *n, t_token **tokens, t_data *data)
 	}
 	return (0);
 }
+
 /*
-Funkcja `ft_check_for_dollar` analizuje zawartość ciągu `*word` w poszukiwaniu znaków specjalnych: pojedynczych apostrofów (`'`), znaku dolara (`$`), oraz podwójnych cudzysłowów (`"`). W przypadku znalezienia: pomija zawartość w apostrofach, przetwarza zmienne środowiskowe za pomocą `ft_dollar`, oraz interpretuje zawartość w cudzysłowach przez `ft_cross_dq`. Funkcja modyfikuje zawartość `*word` zgodnie z kontekstem, zwracając -1 w razie błędu. Jej celem jest przetwarzanie i rozwiązywanie zmiennych środowiskowych i specjalnych sekwencji w analizowanym tekście.
+Funkcja `ft_check_for_dollar` analizuje zawartość ciągu `*word` 
+w poszukiwaniu znaków specjalnych: pojedynczych apostrofów (`'`), 
+znaku dolara (`$`), oraz podwójnych cudzysłowów (`"`). 
+W przypadku znalezienia: pomija zawartość w apostrofach, 
+przetwarza zmienne środowiskowe za pomocą `ft_dollar`, 
+oraz interpretuje zawartość w cudzysłowach przez `ft_cross_dq`. 
+Funkcja modyfikuje zawartość `*word` zgodnie z kontekstem, 
+zwracając -1 w razie błędu. Jej celem jest przetwarzanie 
+i rozwiązywanie zmiennych środowiskowych 
+i specjalnych sekwencji w analizowanym tekście.
 */
 int	ft_check_for_dollar(char **word, t_data *data)
 {
@@ -214,8 +298,16 @@ int	ft_check_for_dollar(char **word, t_data *data)
 	}
 	return (0);
 }
+
 /*
-Funkcja `ft_cross_word` przetwarza słowo, iterując po jego znakach. Jeśli natrafi na cudzysłów (pojedynczy lub podwójny), wywołuje funkcję `ft_clear_quote`, aby usunąć znaki cytatu. Jeśli napotka białe znaki (spacja, tabulator, itp.), wywołuje `ft_cut_token`, aby podzielić słowo na tokeny; jeśli zwróci błąd lub zakończy proces, odpowiednio przerywa działanie. W przypadku innych znaków po prostu przechodzi do następnego. Funkcja kończy się zwróceniem `1`, jeśli przetworzenie się powiodło.
+Funkcja `ft_cross_word` przetwarza słowo, iterując po jego znakach. 
+Jeśli natrafi na cudzysłów (pojedynczy lub podwójny), 
+wywołuje funkcję `ft_clear_quote`, aby usunąć znaki cytatu. 
+Jeśli napotka białe znaki (spacja, tabulator, itp.), 
+wywołuje `ft_cut_token`, aby podzielić słowo na tokeny; 
+jeśli zwróci błąd lub zakończy proces, odpowiednio przerywa działanie. 
+W przypadku innych znaków po prostu przechodzi do następnego. 
+Funkcja kończy się zwróceniem `1`, jeśli przetworzenie się powiodło.
 */
 int	ft_cross_word(char **word, t_token **tokens)
 {
@@ -245,7 +337,20 @@ int	ft_cross_word(char **word, t_token **tokens)
 }
 
 /*
-Funkcja `ft_cut_token` przetwarza słowo, tworząc jego token. Zaczyna od alokacji pamięci na nowy ciąg znaków `tok_str` i kopiuje część `*word` do tego ciągu. Następnie wywołuje `create_token`, by utworzyć token typu `T_WORD`. Jeśli to się nie uda, zwraca błąd i zwalnia pamięć. Następnie funkcja ignoruje białe znaki, aż napotka kolejny token lub koniec słowa. Jeśli nie ma więcej znaków, zwalnia pamięć i kończy. Jeśli napotka kolejne znaki, alokuje nową pamięć dla reszty słowa, kopiuje ją do `new_word`, aktualizuje `*word`, a indeks `i` ustawia na 0, by przetwarzać nowe słowo. Funkcja zwraca `1`, gdy słowo jest kontynuowane, lub `0`, gdy nie ma nic więcej do przetworzenia.
+Funkcja `ft_cut_token` przetwarza słowo, 
+tworząc jego token. 
+Zaczyna od alokacji pamięci na nowy ciąg znaków `tok_str` 
+i kopiuje część `*word` do tego ciągu. Następnie wywołuje `create_token`, 
+by utworzyć token typu `T_WORD`. 
+Jeśli to się nie uda, zwraca błąd i zwalnia pamięć. 
+Następnie funkcja ignoruje białe znaki, 
+aż napotka kolejny token lub koniec słowa. 
+Jeśli nie ma więcej znaków, zwalnia pamięć i kończy. 
+Jeśli napotka kolejne znaki, 
+alokuje nową pamięć dla reszty słowa, kopiuje ją do `new_word`, 
+aktualizuje `*word`, 
+a indeks `i` ustawia na 0, by przetwarzać nowe słowo. Funkcja zwraca `1`, 
+gdy słowo jest kontynuowane, lub `0`, gdy nie ma nic więcej do przetworzenia.
 */
 int	ft_cut_token(int *i, char **word, t_token **tokens)
 {
