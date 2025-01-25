5996c6319465e244ec3e8ee1f80ece6f6db664c4
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-25 14:52:34 +0100

    norminette ok

--- src/builtin_pwd.c
+++ src/builtin_pwd.c
@@ -6,13 +6,19 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 14:46:27 by mbany             #+#    #+#             */
-/*   Updated: 2025/01/19 15:04:44 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/25 14:52:10 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 /*
-Funkcja `pwd_bltin` realizuje działanie wbudowanej komendy `pwd`, która wypisuje aktualną ścieżkę roboczą. Używa funkcji `getcwd` do pobrania bieżącej ścieżki, a jeśli wystąpi błąd, wywołuje funkcję `ft_perror_message` i kończy działanie z kodem błędu `1`. Jeśli ścieżka zostanie pobrana pomyślnie, wypisuje ją na standardowe wyjście. Funkcja kończy działanie z kodem `0`. Jest częścią projektu *minishell*, odwzorowując polecenie `pwd` w interpreterze komend.
+Funkcja `pwd_bltin` realizuje działanie wbudowanej komendy `pwd`,
+ która wypisuje aktualną ścieżkę roboczą. Używa funkcji `getcwd` 
+ do pobrania bieżącej ścieżki, a jeśli wystąpi błąd, wywołuje funkcję 
+ `ft_perror_message` i kończy działanie z kodem błędu `1`. 
+ Jeśli ścieżka zostanie pobrana pomyślnie, wypisuje ją na standardowe wyjście. 
+ Funkcja kończy działanie z kodem `0`. Jest częścią projektu *minishell*, 
+ odwzorowując polecenie `pwd` w interpreterze komend.
 */
 void	pwd_bltin(void)
 {
