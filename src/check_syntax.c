9e75d7e7c8a9c7f08e9ff205e5e4fccfe54e26a8
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-25 15:37:38 +0100

    NORMINETTE OK BUT TOO MANY FUNCTIONS

--- src/check_syntax.c
+++ src/check_syntax.c
@@ -6,66 +6,70 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 14:22:39 by mbany             #+#    #+#             */
-/*   Updated: 2024/12/28 16:37:30 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/25 15:36:56 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "../includes/minishell.h"
+
 static int	is_accepted_char(char ch)
 {
-	if (ch == 33 || ch == 61 || ch == 63 || (ch >=35 && ch <= 47))
+	if (ch == 33 || ch == 61 || ch == 63 || (ch >= 35 && ch <= 47))
 		return (1);
 	else
 		return (0);
 }
+
 int	check_for_preceding_command(char *line, int i)
 {
 	char	init_char;
 
 	init_char = line[i];
-	while ( i >= 0)
+	while (i >= 0)
 	{
 		i--;
-		if (i && ((line[i] == '<' && init_char == '<') || (line[i] == '>' && init_char == '>')))
-			continue;
+		if (i && ((line[i] == '<' && init_char == '<')
+				|| (line[i] == '>' && init_char == '>')))
+			continue ;
 		else if (i && ft_isspace(line[i]))
-			continue;
-		else if ( i && (line[i] == '"' || line[i] == '\''))
+			continue ;
+		else if (i && (line[i] == '"' || line[i] == '\''))
 			go_to_next_quote(line, &i, true);
 		else if (ft_isalnum(line[i]) || is_accepted_char(line[i]))
 			return (0);
 		else
-			break;
+			break ;
 	}
 	return (-1);
 }
 
-int check_for_following_command(char *line,int i)
+int	check_for_following_command(char *line, int i)
 {
 	char	org_char;
+
 	org_char = line[i];
 	i++;
 	while (line[i] != '\0')
 	{
 		if (ft_isspace(line[i]))
 			i++;
-		else if (ft_isalnum(line[i]) 
-		|| (line[i] == '>' && org_char != '>') 
-		|| (line[i] == '<' && org_char != '<') 
-		|| (line[i] == '/' && (org_char == '<' || org_char == '>' || org_char == '|')) 
-		|| (line[i] == '$') || (line[i] == '.') 
-		|| (line[i] == '"') || (line[i] == '\''))
+		else if (ft_isalnum(line[i])
+			|| (line[i] == '>' && org_char != '>')
+			|| (line[i] == '<' && org_char != '<')
+			|| (line[i] == '/' && (org_char == '<'
+					|| org_char == '>' || org_char == '|'))
+			|| (line[i] == '$') || (line[i] == '.')
+			|| (line[i] == '"') || (line[i] == '\''))
 			return (0);
 		else
-			break;
+			break ;
 	}
 	return (-1);
 }
 
 int	check_for_missing_command(char *line)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (line[i] == '\0')
@@ -76,10 +80,11 @@ int	check_for_missing_command(char *line)
 		{
 			if (check_for_following_command(line, i))
 				return (-1);
-			if(check_for_preceding_command(line, i))
+			if (check_for_preceding_command(line, i))
 				return (-1);
 		}
-		else if ((line[i] == '>' && line[i+1] != '>') || (line[i] == '<' && line[i +1] != '<'))
+		else if ((line[i] == '>' && line[i + 1] != '>')
+			|| (line[i] == '<' && line[i + 1] != '<'))
 		{
 			if (check_for_following_command(line, i))
 				return (-1);
@@ -89,11 +94,11 @@ int	check_for_missing_command(char *line)
 	return (0);
 }
 
-int check_for_unclosed_quotes(char *line)
+int	check_for_unclosed_quotes(char *line)
 {
-	int i;
-	bool in_quote;
-	char quote;
+	int		i;
+	bool	in_quote;
+	char	quote;
 
 	i = 0;
 	in_quote = false;
@@ -117,12 +122,12 @@ int check_for_unclosed_quotes(char *line)
 	return (0);
 }
 
-int check_if_line_is_empty(char *line)
+int	check_if_line_is_empty(char *line)
 {
-	char *trimmed_line;
+	char	*trimmed_line;
 
 	trimmed_line = ft_strtrim(line, "\t");
-	if(!trimmed_line)
+	if (!trimmed_line)
 	{
 		perror("check_if_line_is_empty");
 		return (1);
@@ -135,7 +140,8 @@ int check_if_line_is_empty(char *line)
 	free(trimmed_line);
 	return (0);
 }
-int check_syntax(char *line)
+
+int	check_syntax(char *line)
 {
 	if (check_if_line_is_empty(line))
 		return (-1);
