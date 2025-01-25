2d586eded759d5fe8d3e560b0e9a865271795f0b
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-25 16:59:41 +0100

    norminette ok

--- src/error.c
+++ src/error.c
@@ -6,11 +6,12 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 15:24:26 by mbany             #+#    #+#             */
-/*   Updated: 2025/01/05 15:34:44 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/25 16:59:04 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
 void	msg_error(char *err)
 {
 	write(2, err, ft_strlen(err));
@@ -23,6 +24,7 @@ int	ft_error_message(char *str, int num)
 	ft_putstr_fd("\n", 2);
 	return (num);
 }
+
 int	ft_perror_message(void)
 {
 	perror(NULL);
@@ -30,7 +32,13 @@ int	ft_perror_message(void)
 }
 
 /*
-Funkcja `ft_perror_free` wypisuje komunikat o błędzie za pomocą `ft_perror_message`, a następnie zwalnia pamięć wskazywaną przez wskaźniki `first`, `second` i `third`, jeśli nie są one puste. Na końcu zwraca `-1`, aby zasygnalizować błąd. Funkcja służy do obsługi błędów i jednoczesnego czyszczenia zaalokowanej pamięci, zapobiegając wyciekom.
+Funkcja `ft_perror_free` wypisuje komunikat 
+o błędzie za pomocą `ft_perror_message`, 
+a następnie zwalnia pamięć wskazywaną 
+przez wskaźniki `first`, `second` i `third`, 
+jeśli nie są one puste. Na końcu zwraca `-1`, aby zasygnalizować błąd. 
+Funkcja służy do obsługi błędów i jednoczesnego czyszczenia 
+zaalokowanej pamięci, zapobiegając wyciekom.
 */
 int	ft_perror_free(char *first, char *second, char *third)
 {
