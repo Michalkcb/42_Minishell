7a40b603609ece169e017b2a9f8c56d7fd099482
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-01-26 11:21:36 +0100

    compilation errors fixed

--- src/sanitize01.c
+++ src/sanitize01.c
@@ -6,54 +6,32 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 13:34:28 by mbany             #+#    #+#             */
-/*   Updated: 2025/01/26 10:21:31 by mbany            ###   ########.fr       */
+/*   Updated: 2025/01/26 11:05:41 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
 /*
-Funkcja `process_str` przetwarza wejściowy ciąg znaków `str` 
-i zapisuje zaktualizowaną wersję w `str_final`. 
-Iteruje przez `str`, pomijając zbędne spacje 
-i tabulatory oraz obsługując specjalne przypadki, 
-takie jak cudzysłowy (`handle_quotes`) 
-i operatory (`handle_operators`). 
-Jeśli bufor `str_final` staje się za mały, 
-jego rozmiar jest zwiększany za pomocą `realloc_str_final`. 
-Funkcja kończy przetwarzanie, 
-dodając znak null (`\0`) na końcu ciągu i zwracając wynik. 
-Jej celem jest normalizacja i interpretacja wejściowego 
-ciągu znaków w kontekście składni poleceń.
+Funkcja `optimize_str_final` tworzy nową 
+kopię łańcucha znaków `str_final` za pomocą `ft_strdup`, 
+zwalnia pamięć zajmowaną przez oryginalny łańcuch 
+i zwraca wskaźnik do nowej kopii. 
+Służy do zarządzania pamięcią, 
+aby zastąpić oryginalny wskaźnik nowym, 
+niezależnym łańcuchem znaków.
 */
-char	*process_str(char *str, char *str_final,
-int str_final_len)
+static char	*optimize_str_final(char *str_final)
 {
-	int	i;
-	int	j;
+	char	*new_str;
 
-	i = 0;
-	j = 0;
-	while (str[i] != '\0')
-	{
-		if (j + 1 >= str_final_len)
-			str_final_len = realloc_str_final(&str_final, j, str_final_len);
-		if (str_final_len == -1)
-			return (NULL);
-		if (str[i] == '"' || str[i] == '\'')
-			handle_quotes(str, str_final, &i, &j);
-		else if (ft_isspace(str[i]) && (i == 0 || ft_isspace(str[i + 1])))
-			i++;
-		else if
-			((ft_isalnum(str[i]) && is_operator(str[i + 1]))
-			|| (str[i] == '|' && (str[i + 1] == '$'
-			|| str[i + 1] == '<' || str[i + 1] == '>' )))
-			handle_operators(str, str_final, &i, &j);
-		else
-			str_final[j++] = str[i++];
-	}
-	str_final[j] = '\0';
-	return (str_final);
+	if (!str_final)
+		return (NULL);
+	new_str = ft_strdup(str_final);
+	free(str_final);
+	if (!new_str)
+		return (NULL);
+	return (new_str);
 }
 
 /*
